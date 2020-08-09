d9f62fc0ba8d3fc52051e0e7c3b1f4213f3e1a98
mohammad <mohammad.alayoub@mni.thm.de>
2020-08-09 03:13:24 +0200

    changed

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const PORT = process.env.PORT || 1212;
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html')
-    //res.render('test.jade');
 });
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function () {
-    console.log("app running at localhost:1212");
+    console.log("|-------------------------------------------------------|\n" +
+                "|                                                       |\n" +
+                "|             app running at localhost:1212             |\n" +
+                "|                                                       |\n" +
+                "|-------------------------------------------------------|\n");
 });
 
